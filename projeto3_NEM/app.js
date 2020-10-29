7147b88d2559e8a3dd7c12cbed606c1a6c7c480d
decorocco <andrebr@al.insper.edu.br>
2020-10-29 09:14:30 -0300

    app.js mudanças

--- projeto3_NEM/app.js
+++ projeto3_NEM/app.js
@@ -1,18 +1,16 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const cors = require('cors')
 const express = require('express')
-const app = express()
 server.use(cors())
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
