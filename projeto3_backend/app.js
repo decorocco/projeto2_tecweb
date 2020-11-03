56439f7c15789b9862dc3f6e1e7fe96e89412cc5
decorocco <andrebr@al.insper.edu.br>
2020-11-03 09:12:10 -0300

    servidor mongo local -> atlas

--- projeto3_backend/app.js
+++ projeto3_backend/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/projeto3_backend', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect("mongodb+srv://andrebr:6yO5wVVOl12F@cluster0.0qwku.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
 	console.log("MongoDB database connection established successfully");
 });
-
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
